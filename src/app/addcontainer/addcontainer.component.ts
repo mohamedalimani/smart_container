5570d3cad0bd13c1818d760b1bf605f0d2faed7e
Manix <52538269+mohamedalimani@users.noreply.github.com>
2021-02-22 21:41:08 +0100

    smart container
    dunno

---
+++ src/app/addcontainer/addcontainer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addcontainer',
+  templateUrl: './addcontainer.component.html',
+  styleUrls: ['./addcontainer.component.css']
+})
+export class AddcontainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
+import { AddcontainerComponent } from './addcontainer/addcontainer.component';
+import { MapComponent } from './map/map.component';
+
+const root:Routes = [{path:'',component:MainComponent},
+                     {path:'user',component:UserComponent}, 
+                     {path:'signup',component:SignupComponent},
+                     {path:'add',component:AddcontainerComponent},   
+                     {path:'**',component:NotFoundComponent}] ;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    UserComponent,
+    NotFoundComponent,
+    SignupComponent,
+    AddcontainerComponent,
+    MapComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule,
+            RouterModule.forRoot(root)
+          ],
   providers: [],
   bootstrap: [AppComponent]
 })
